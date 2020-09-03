a4ea9132d1a63ac2443310c62636d5e554ea5155
mnmlsniper <mnml.sniper@gmail.com>
2020-09-03 14:41:02 +0300

    add demo axios request

--- tests/demo.js
+++ tests/demo.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 jest.setTimeout(50000);
 
 describe('demo', () => {
-    test('demo get', async () => {
-        expect(1+1).toBe(2);
+    test('demo get nasa site', async () => {
+        const r = await axios.get('https://api.nasa.gov/');
+        expect(r.status).toBe(200);
     });
 });
